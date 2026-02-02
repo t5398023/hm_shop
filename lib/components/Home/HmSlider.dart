7d86b733a21774850a151891f0b1301dec5fb176
tandeng <1069271596@qq.com>
2026-02-02 14:37:38 +0800

    轮播图搜索框和指示灯导航

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,6 +11,7 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  int _currentIndex = 0;
   Widget _getSlider(){
     final double screenWidth = MediaQuery.of(context).size.width;
     return CarouselSlider(
@@ -18,14 +19,64 @@ class _HmSliderState extends State<HmSlider> {
       options: CarouselOptions(
         viewportFraction: 1.0,
         autoPlay: true,
+        onPageChanged: (index, reason) {
+          setState(() {
+            _currentIndex = index;
+          });
+        }
+
 
     ));
+  }
+  Widget _getSearch(){
+    return Positioned(
+      top: 10.0,
+      left: 10.0,
+      right: 10.0,
+      child: Container(
+        height: 50,
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(95, 0, 0, 0),
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.search,color: Colors.white,),
+            SizedBox(width: 10.0,),
+            Text("搜索",style: TextStyle(color: Colors.white,fontSize: 16.0),),
+          ],
+        ),
+      ),
+    );
+  } 
+  Widget _getDots(){
+    return Positioned(
+      bottom: 10.0,
+      left: 0.0,
+      right: 0.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(widget.banners.length, (index) => AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          width: _currentIndex == index ? 20.0 : 10.0,
+          height: 10.0,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(_currentIndex == index ? 1.0 : 0.5),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        )),
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         _getSlider(),
+        _getSearch(),  
+        _getDots(),
       ]
     );
   }
